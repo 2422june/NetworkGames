b9bc50e56afd82d333ae5fc85cad25c130b1a621
2422june <ck66661254@gmail.com>
2023-10-24 12:19:33 +0900

    Init Asset

---
+++ NetworkGames/Assets/1.Assets/MetroidvaniaController/Scripts/KillZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SM = UnityEngine.SceneManagement.SceneManager;
+
+public class KillZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SM.LoadSceneAsync(SM.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
